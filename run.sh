9d96ef8347bd7fb9bcfbdb919e8bea78fd65b5c4
Wendelin <wendelin.boehmer@cs.ox.ac.uk>
2018-04-27 18:41:24 +0100

    fix naming

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 GPU=$1
-name=${USER}_deepmarl_GPU_${GPU}_${HASH}
+name=${USER}_pymarl_GPU_${GPU}_${HASH}
 
 echo "Launching container named '${name}' on GPU '${GPU}'"
 # Launches a docker container using our image, and runs the provided command
