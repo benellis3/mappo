9fb858e703331a3ab30059f8eea6e82db05d54ee
Christian Schroeder <caschroeder@outlook.com>
2018-04-30 00:07:11 +0100

    modified pomace exp run scripts

--- exp_scripts/pomace_exp1_exp2/run_pp.sh
+++ exp_scripts/pomace_exp1_exp2/run_pp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NREPS=$1
-./exp1_pp.sh  ${NREPS} & ./exp1_pp.sh with pomace_exp_variant=2 & ./exp2_pp.sh with pomace_exp_variant=1 ${NREPS} & ./exp2_pp.sh with pomace_exp_variant=2
+./exp1_pp.sh ${NREPS} & ./exp2_pp.sh ${NREPS}
