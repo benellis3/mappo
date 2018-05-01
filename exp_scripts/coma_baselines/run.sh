5c56ce0192400ad260064abcc3c6fa65412c820c
Christian Schroeder <caschroeder@outlook.com>
2018-05-01 13:02:35 +0100

    minor fix to coma baseline scripts

--- exp_scripts/coma_baselines/run.sh
+++ exp_scripts/coma_baselines/run.sh
@@ -4,4 +4,4 @@ TAG=$1
 N_REPEAT=$2
 TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
-./baseline1.sh ${TAG} ${TIMESTAMP} ${NREPS} & ./baseline2.sh ${TAG} ${TIMESTAMP} ${NREPS} & ./baseline3.sh ${TAG} ${TIMESTAMP} ${NREPS}
+./baseline1.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} & ./baseline2.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} & ./baseline3.sh ${TAG} ${TIMESTAMP} ${N_REPEAT}
