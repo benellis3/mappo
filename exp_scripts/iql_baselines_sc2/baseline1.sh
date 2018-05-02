2c8dd66391a24ae12b05ce6a8c8bf7b297fc6fb4
Christian Schroeder <caschroeder@outlook.com>
2018-05-02 14:36:52 +0100

    added run scripts for iql sc2 baselines

---
+++ exp_scripts/iql_baselines_sc2/baseline1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# To be called from inside Docker container for now
+
+#cd /pymarl/src
+export PYTHONPATH=$PYTHONPATH:/pymarl/src
+
+TAG=$1
+TIMESTAMP=$2
+N_REPEAT=$3
+N_GPUS=`nvidia-smi -L | wc -l`
+N_UPPER=`expr $N_GPUS - 1`
+
+for i in $(seq 1 $N_REPEAT); do
+  GPU_ID=`shuf -i0-${N_UPPER} -n1`
+  echo "Starting repeat number $i on GPU $GPU_ID"
+  NV_GPU=${GPU_ID} ../.././docker.sh python3 /pymarl/src/main.py --exp_name=iql_baseline_3m with name=${TAG}__iql_baseline_3m_${TIMESTAMP}__repeat${i} &
+  sleep 10s
+done
