77fb3624e7357ab7f897d39069c7fdfb6cfde3f7
timrudner <tim.rudner@gmail.com>
2018-05-03 00:44:30 +0100

    fix small errors in previous push

--- src/components/env_stats_aggregators.py
+++ src/components/env_stats_aggregators.py
@@ -1,7 +1,7 @@
 REGISTRY = {}
 
 from envs.starcraft1 import StatsAggregator as SC1StatsAggregator
-REGISTRY["sc2"] = SC1StatsAggregator
+REGISTRY["sc1"] = SC1StatsAggregator
 
 from envs.starcraft2 import StatsAggregator as SC2StatsAggregator
 REGISTRY["sc2"] = SC2StatsAggregator
