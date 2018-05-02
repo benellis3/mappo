36130b79664c37642ee8aa26bf2ba775692e45b6
Christian Schroeder <caschroeder@outlook.com>
2018-05-02 22:51:26 +0100

    minor baseline fix

--- exp_scripts/pomace_exp1_var2_pp/baseline1.sh
+++ exp_scripts/pomace_exp1_var2_pp/baseline1.sh
@@ -13,6 +13,6 @@ N_UPPER=`expr $N_GPUS - 1`
 for i in $(seq 1 $N_REPEAT); do
   GPU_ID=`shuf -i0-${N_UPPER} -n1`
   echo "Starting repeat number $i on GPU $GPU_ID"
-  NV_GPU=${GPU_ID} ../.././docker.sh python3 /pymarl/src/main.py ---exp_name="pomace_exp1_coma_pp"  with name=${TAG}__pomace_exp1_coma_pp_${TIMESTAMP}__repeat${i} pomace_exp_variant=2 use_tensorboard=False &
+  NV_GPU=${GPU_ID} ../.././docker.sh python3 /pymarl/src/main.py --exp_name="pomace_exp1_coma_pp"  with name=${TAG}__pomace_exp1_coma_pp_${TIMESTAMP}__repeat${i} pomace_exp_variant=2 use_tensorboard=False &
   sleep 10s
 done
