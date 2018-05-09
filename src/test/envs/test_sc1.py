34573f3b3920bd64bd4988a9fc5c18f6529f64af
ascane <mandy00724@gmail.com>
2018-05-09 10:08:02 +0100

    Initial setup.

--- src/test/envs/test_sc1.py
+++ src/test/envs/test_sc1.py
@@ -1,8 +1,16 @@
-from runners.nstep_runner import NStepRunner
-from utils.dict2namedtuple import convert
+import torch as th
+import numpy as np
+from envs import REGISTRY as envs_REGISTRY
+# import envs.starcraft1.SC1 as SC1
+
+import sys
+print(sys.path)
 
 def test_connection():
-
+    print("testing sc1...")
+    kwargs = dict()
+    sc1 = envs_REGISTRY["sc1"](kwargs)
+    # sc1 = SC1(kwargs)
     pass
 
 def main():
