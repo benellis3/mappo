6de00d0f7ad8b5144e811b64146a7e2a667e89da
Tabish Rashid <tabzraz@gmail.com>
2018-05-11 23:01:45 +0100

    Adding files for running experiments in a nicer way. Doesn't quite work yet (it seems to execute run.sh on the local servers but then does nothing, ngc is untested). An example is in exp_scripts/script_test, can be run by doing the following: 1) Be in exp_scripts 2) python3 run_experiment.py script_test.pp_test

---
+++ exp_scripts/execute_on_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Kills the existing docker container and restarts a new detached container
+
+GPU_ID=$1
+N_REPEAT=$2
+PARAMS="${@:3}"
+
+# I am not sshed into the server in my home directory
+
+if [ -z "$EXP_DIR" ]
+then
+    EXP_DIR=~
+fi
+
+echo "EXP_DIR: $EXP_DIR"
+
+# Update the git repo
+echo "Updating git repo"
+#mkdir -p $EXP_DIR/deepmarl
+cd $EXP_DIR/pymarl
+# echo "REMEMBER TO ALLOW UPDATING OF THE REPO IN execute_on_server.sh AFTER TESTING!"
+git fetch -q origin
+
+# Run the experiment $N_REPEAT times on GPU $GPU
+bash ./run.sh $GPU_ID "bash scripts/repeat_exp.sh $N_REPEAT $PARAMS"
---
+++ exp_scripts/reset_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Kills all docker containers running the deepmarl/pytorch image.
+# Rebuilds docker image
+
+if [ -z "$EXP_DIR" ]
+then
+    EXP_DIR=~
+fi
+
+echo "EXP_DIR: $EXP_DIR"
+cd $EXP_DIR
+
+# Check for the deepmarl repo, if it is not there then clone it
+if [ ! -d "pymarl" ]; then
+    echo "Cloning pymarl repo"
+    # If this doesn't work on a server you might have to manually connect and clone the repo for the first time
+    git clone git@github.com:oxwhirl/pymarl.git
+fi
+
+cd $EXP_DIR/pymarl
+
+git fetch -q origin
+
+# Kill the docker container
+./kill.sh
+
+# Rebuild the docker container
+./build.sh
