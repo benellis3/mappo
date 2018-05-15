d4e6020be2d4c8275374dbb81946e07be2c19157
Christian Schroeder <cs@robots.ox.ac.uk>
2018-05-15 17:46:52 +0100

    annoying fix

--- exp_scripts/coma_wendelin_pp/wendelin_3agent_4x4.sh
+++ exp_scripts/coma_wendelin_pp/wendelin_3agent_4x4.sh
@@ -13,6 +13,6 @@ N_UPPER=`expr $N_GPUS - 1`
 for i in $(seq 1 $N_REPEAT); do
   GPU_ID=`shuf -i0-${N_UPPER} -n1`
   echo "Starting repeat number $i on GPU $GPU_ID"
-  NV_GPU=${GPU_ID} ../.././docker.sh python3 /pymarl/src/main.py --exp_name="coma_3agent_4x4" with name=${TAG}__coma_3agent_4x4__${TIMESTAMP}__repeat${i} use_tensorboard=False &
+  NV_GPU=${GPU_ID} ../.././docker.sh python3 /pymarl/src/main.py --exp_name="coma_wendelin_3agent_4x4" with name=${TAG}__coma_wendelin_3agent_4x4__${TIMESTAMP}__repeat${i} use_tensorboard=False &
   sleep 10s
 done
