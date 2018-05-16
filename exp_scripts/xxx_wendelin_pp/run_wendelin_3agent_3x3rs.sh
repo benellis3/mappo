778d1bfe97743149cbd778ce1a3f5e1461d43f11
Christian Schroeder <caschroeder@outlook.com>
2018-05-16 22:00:32 +0100

    added JAKOB scripts for 3agent 3x3 myopic experiments

---
+++ exp_scripts/xxx_wendelin_pp/run_wendelin_3agent_3x3rs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TAG=$1
+N_REPEAT=$2
+TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
+echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
+./wendelin_3agent_3x3rs.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
