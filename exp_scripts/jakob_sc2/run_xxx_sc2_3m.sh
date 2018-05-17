0bca9f13995ca40fe979cef89def0cfdf152c545
Christian Schroeder <caschroeder@outlook.com>
2018-05-17 20:04:37 +0100

    finished exp run script JAKOB SC2 + implemented Mongo fail exit

--- exp_scripts/jakob_sc2/run_xxx_sc2_3m.sh
+++ exp_scripts/jakob_sc2/run_xxx_sc2_3m.sh
@@ -5,4 +5,4 @@ N_REPEAT=$2
 TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
 #./coma_jakob_sc2.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
-./xxx_jakob_sc2.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
+./xxx_jakob_sc2_3m.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
