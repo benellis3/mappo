da9ab6a69a5b26184cd4a005eea8aea31b80282c
Christian Schroeder <caschroeder@outlook.com>
2018-05-18 14:47:55 +0100

    fix

--- exp_scripts/jakob_sc2_sf16/run_coma_sc2_3m_sf16.sh
+++ exp_scripts/jakob_sc2_sf16/run_coma_sc2_3m_sf16.sh
@@ -4,4 +4,4 @@ TAG=$1
 N_REPEAT=$2
 TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
-./coma_jakob_sc2_sf16.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
+./coma_jakob_sc2_3m_sf16.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
