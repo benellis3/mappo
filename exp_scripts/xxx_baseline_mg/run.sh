424893f8e84cf56d56f4336c6bcb8009a13cf7df
Your Name <you@example.com>
2018-05-18 03:04:37 +0100

    tiny

--- exp_scripts/xxx_baseline_mg/run.sh
+++ exp_scripts/xxx_baseline_mg/run.sh
@@ -4,4 +4,4 @@ TAG=$1
 N_REPEAT=$2
 TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
-exp1.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} & exp1.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
+./exp1.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} & ./exp2.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} & ./exp3.sh ${TAG} ${TIMESTAMP} ${N_REPEAT}
