9315d9e0897d9586081e1396317b6dd3bf827213
Christian Schroeder <cs@robots.ox.ac.uk>
2018-05-22 21:10:26 +0100

    minor fix

--- exp_scripts/mackrel/jakob_hyper_sc2/xxx_jakob_sc2_3m__lra_hyper.sh
+++ exp_scripts/mackrel/jakob_hyper_sc2/xxx_jakob_sc2_3m__lra_hyper.sh
@@ -17,6 +17,6 @@ N_UPPER=`expr $N_GPUS - 1`
 for i in $(seq 1 $N_REPEAT); do
   GPU_ID=`shuf -i0-${N_UPPER} -n1`
   echo "Starting repeat number $i on GPU $GPU_ID"
-  NV_GPU=${GPU_ID} ../../.././docker.sh python3 /pymarl/src/main.py --exp_name="xxx_jakob_sc2_3m" with batch_size=7 batch_size_run=7 lr_agent_level1=0.001 lr_agent_level2=0.001 lr_agent_level3=0.001 name=${TAG}__${filename}__${TIMESTAMP}__repeat${i} use_tensorboard=False &
+  NV_GPU=${GPU_ID} ../../.././docker.sh python3 /pymarl/src/main.py --exp_name="mackrel/jakob_sc2_3m" with batch_size=7 batch_size_run=7 lr_agent_level1=0.001 lr_agent_level2=0.001 lr_agent_level3=0.001 name=${TAG}__${filename}__${TIMESTAMP}__repeat${i} use_tensorboard=False &
   sleep 10s
 done
