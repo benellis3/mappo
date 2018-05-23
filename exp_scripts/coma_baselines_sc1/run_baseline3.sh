690daa475ace5fc8f6c59dff68909be1ff7671ac
Christian Schroeder <caschroeder@outlook.com>
2018-05-23 23:15:12 +0100

    updated sc1 baseline experiment scripts

--- exp_scripts/coma_baselines_sc1/run_baseline3.sh
+++ exp_scripts/coma_baselines_sc1/run_baseline3.sh
@@ -4,4 +4,5 @@ TAG=$1
 N_REPEAT=$2
 TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 echo "Experiment ${TAG} time stamp: ${TIMESTAMP}"
-./baseline2.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
+./baseline3.sh ${TAG} ${TIMESTAMP} ${N_REPEAT} &
+
