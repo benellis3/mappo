77ebf58c048604c6e408c394fdcf02ee281877af
Christian Schroeder <caschroeder@outlook.com>
2018-06-28 12:01:59 +0100

    fixed full-observability intersections

--- exp_scripts/flounderl/meeting280618/flounderl_jakob_pp_4agents_6x6__fo.sh
+++ exp_scripts/flounderl/meeting280618/flounderl_jakob_pp_4agents_6x6__fo.sh
@@ -9,14 +9,14 @@ extension="${filename##*.}"
 filename="${filename%.*}"
 
 TAG=$1
-TIMESTAMP=$2
-N_REPEAT=$3
+N_REPEAT=$2
+TIMESTAMP=`date "+%d/%m/%y-%H:%M:%S"`
 N_GPUS=`nvidia-smi -L | wc -l`
 N_UPPER=`expr $N_GPUS - 1`
 
 for i in $(seq 1 $N_REPEAT); do
   GPU_ID=`shuf -i0-${N_UPPER} -n1`
   echo "Starting repeat number $i on GPU $GPU_ID"
-  NV_GPU=${GPU_ID} ../../.././docker.sh python3 /pymarl/src/main.py --exp_name="flounderl/pp_4agent_6x6" with name=${TAG}__${filename}__${TIMESTAMP}__repeat${i} use_tensorboard=False&
+  NV_GPU=${GPU_ID} ../../.././docker.sh python3 /pymarl/src/main.py --exp_name="flounderl/pp_4agent_6x6" with name=${TAG}__${filename}__${TIMESTAMP}__repeat${i} use_tensorboard=False &
   sleep 10s
 done
