51189db2edab9426b6e4e4f915d60eecd74367c5
Your Name <you@example.com>
2018-07-10 16:10:07 +0100

    upgraded NGC docker version

--- build_ngc.sh
+++ build_ngc.sh
@@ -1,3 +1,5 @@
 echo 'Building Dockerfile with image name pymarl/ngc`'
 #docker build --no-cache -t deepmarl/pytorch .
+mv .dockerignore .dockerignore.tmp
 docker build -t pymarl/ngc -f Dockerfile.ngc .
+mv .dockerignore.tmp .dockerignore
