0a8607b86e9a68c3e3f1fed3269d000cd09c55f5
Tabish Rashid <tabzraz@gmail.com>
2018-07-25 00:51:40 +0100

    qmix test script for 3m and 5m

--- exp_scripts/qmix_sc2_test/qmix_test.py
+++ exp_scripts/qmix_sc2_test/qmix_test.py
@@ -1,12 +1,12 @@
 from run_experiment import extend_param_dicts
 
 server_list = [
-    ("brown", [0,1,2,3,4,5,6,7], 1),
+    ("brown", [0,1,2,3,4,5,6,7], 2),
 ]
 
 LABEL = "QMIX_sc2_test_24July2018"
 
-n_repeat = 4
+n_repeat = 2
 
 param_dicts = []
 
@@ -18,5 +18,12 @@ shared_params = {
 extend_param_dicts(param_dicts, shared_params,
     {
         "name": "qmix_sc2_3m"
+    },
+    repeats=8)
+
+extend_param_dicts(param_dicts, shared_params,
+    {
+        "name": "qmix_sc2_5m",
+        "env_args.map_name": "5m_5m"
     },
     repeats=8)
