782dafce3fa67588ecf8f149f514c71b3e84be80
Tabish Rashid <tabzraz@gmail.com>
2018-07-24 20:25:57 +0100

    qmix experiment script for testing 3m_3m

--- exp_scripts/qmix_sc2_test/qmix_test.py
+++ exp_scripts/qmix_sc2_test/qmix_test.py
@@ -1,7 +1,7 @@
 from run_experiment import extend_param_dicts
 
 server_list = [
-    ("dgx1", [0,1,2,3,4,5,6,7], 1),
+    ("brown", [0,1,2,3,4,5,6,7], 1),
 ]
 
 LABEL = "QMIX_sc2_test_24July2018"
@@ -10,14 +10,12 @@ n_repeat = 4
 
 param_dicts = []
 
-exp_name = "coma_baseline_pp"
+exp_name = "qmix/qmix_sc2"
 
 shared_params = {
-    "use_tensorboard": False,
 }
 
 extend_param_dicts(param_dicts, shared_params,
     {
-        "name": "comabaseline_test"
     },
-    repeats=1)
+    repeats=8)
