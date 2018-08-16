4214c592f5df18d104aa1d9c725a22b064daa428
greg <greg@robots.ox.ac.uk>
2018-08-16 17:38:30 +0100

    change some names

--- src/learners/q_learner.py
+++ src/learners/q_learner.py
@@ -99,13 +99,13 @@ class QLearner:
             self._update_targets()
             self.last_target_update_episode = episode_num
 
-        if t_env - self.log_stats_t >= self.args.trainer_log_interval:
+        if t_env - self.log_stats_t >= self.args.learner_log_interval:
             self.logger.log_stat("loss", loss.item(), t_env)
             self.logger.log_stat("grad_norm", grad_norm, t_env)
             mask_elems = mask.sum().item()
-            self.logger.log_stat("abs_td_error", (masked_td_error.abs().sum().item()/mask_elems), t_env)
-            self.logger.log_stat("mean_q_value", (chosen_action_qvals * mask).sum().item()/(mask_elems * self.args.n_agents), t_env)
-            self.logger.log_stat("mean_target", (targets * mask).sum().item()/(mask_elems * self.args.n_agents), t_env)
+            self.logger.log_stat("td_error_abs", (masked_td_error.abs().sum().item()/mask_elems), t_env)
+            self.logger.log_stat("q_taken_mean", (chosen_action_qvals * mask).sum().item()/(mask_elems * self.args.n_agents), t_env)
+            self.logger.log_stat("target_mean", (targets * mask).sum().item()/(mask_elems * self.args.n_agents), t_env)
             self.log_stats_t = t_env
 
     def _update_targets(self):
