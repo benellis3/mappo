51a60bfcdc1e794ea2f97c9a0288647d290cbcbd
greg <greg@robots.ox.ac.uk>
2018-08-20 20:17:56 +0100

    wip coma exp

--- exp_scripts/coma/coma_test.py
+++ exp_scripts/coma/coma_test.py
@@ -1,14 +1,14 @@
 from run_experiment import extend_param_dicts
 
 server_list = [
-    ("bilbo", [0,1,2,3,4,5,6,7], 1),
+    ("gandalf", [0,1,2,3,4,5,6,7], 1),
 ]
 
-label = "COMA_refactor_test_1"
+label = "COMA_refactor_test_2"
 config = "coma"
 env_config = "sc2"
 
-n_repeat = 10
+n_repeat = 5
 
 param_dicts = []
 
@@ -18,17 +18,27 @@ shared_params = {
 
 extend_param_dicts(param_dicts, shared_params,
     {
-        "name": "coma_sc2_3m",
-        "env_args.map_name": "3m_3m"
+        "name": "coma_sc2_5m_critic_lowlr",
+        "lr": 0.0005,
+        "critic_lr": 0.0001,
+        "env_args.map_name": "5m_5m"
     },
     repeats=2)
 
-for lr in [0.0001, 0.0005, 0.001]:
-    extend_param_dicts(param_dicts, shared_params,
-        {
-            "name": "coma_sc2_5m",
-            "lr": lr,
-            "critic_lr": lr,
-            "env_args.map_name": "5m_5m"
-        },
-        repeats=2)
+extend_param_dicts(param_dicts, shared_params,
+    {
+        "name": "coma_sc2_5m_agent_lowlr",
+        "lr": 0.0001,
+        "critic_lr": 0.0005,
+        "env_args.map_name": "5m_5m"
+    },
+    repeats=2)
+
+extend_param_dicts(param_dicts, shared_params,
+    {
+        "name": "coma_sc2_3s_5z",
+        "lr": 0.0005,
+        "critic_lr": 0.0005,
+        "env_args.map_name": "3s_5z"
+    },
+    repeats=4)
