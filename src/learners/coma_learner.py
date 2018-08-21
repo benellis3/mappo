53cf903298059f1ce54fe45e198179e775cfd9b5
greg <greg@robots.ox.ac.uk>
2018-08-21 13:18:59 +0100

    mask_elems

--- src/learners/coma_learner.py
+++ src/learners/coma_learner.py
@@ -146,9 +146,10 @@ class COMALearner:
 
             running_log["critic_loss"].append(loss.item())
             running_log["critic_grad_norm"].append(grad_norm)
-            running_log["td_error_abs"].append((masked_td_error.abs().sum().item() / mask_t.sum().item()))
-            running_log["q_taken_mean"].append((q_taken * mask_t).sum().item() / (mask_t.sum().item()))
-            running_log["target_mean"].append((targets_t * mask_t).sum().item() / mask_t.sum().item())
+            mask_elems = mask_t.sum().item()
+            running_log["td_error_abs"].append((masked_td_error.abs().sum().item() / mask_elems))
+            running_log["q_taken_mean"].append((q_taken * mask_t).sum().item() / mask_elems)
+            running_log["target_mean"].append((targets_t * mask_t).sum().item() / mask_elems)
 
         return q_vals.view(-1, self.n_actions), running_log
 
