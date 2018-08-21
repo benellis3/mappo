35c7018e84cb405f026d76717295f42e466d36fc
Mikayel Samvelyan <mika.samvelyan@gmail.com>
2018-08-21 23:55:13 +0400

    Little bit of SC2 code refactoring

--- src/envs/starcraft2/__init__.py
+++ src/envs/starcraft2/__init__.py
@@ -1,5 +1,5 @@
 from .starcraft2 import SC2 as StarCraft2Env, StatsAggregator
-from .starcraft2 import map_param_registry
+from .map_params import map_param_registry
 from pysc2 import maps
 
 for name in map_param_registry.keys():
