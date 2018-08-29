6233f6e38746b6df17cc70342eed6f5c06a36414
Tim Rudner <tim.rudner@gmail.com>
2018-08-29 08:37:21 -0400

    SC1 5v5m runs+trains on qmix

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -23,12 +23,10 @@ REGISTRY["matrix_game"] = partial(env_fn, env=NormalFormMatrixGame)
 REGISTRY["integration_test"] = partial(env_fn, env=IntegrationTestEnv)
 REGISTRY["box_push"] = partial(env_fn, env=CoopBoxPushing)
 
-try:
-    from .starcraft1 import StarCraft1Env
-    REGISTRY["sc1"] = partial(env_fn,
-                              env=StarCraft1Env)
-except Exception as e:
-    print(e)
+
+from .starcraft1 import StarCraft1Env
+REGISTRY["sc1"] = partial(env_fn,
+                          env=StarCraft1Env)
 
 from .starcraft2 import StarCraft2Env
 REGISTRY["sc2"] = partial(env_fn,
