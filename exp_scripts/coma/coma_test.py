183e851bdf29dc23470f8c3333523ba4f9662667
greg <greg@robots.ox.ac.uk>
2018-08-29 15:41:04 +0100

    coma bugfix test

--- exp_scripts/coma/coma_test.py
+++ exp_scripts/coma/coma_test.py
@@ -1,11 +1,10 @@
 from run_experiment import extend_param_dicts
 
 server_list = [
-    ("gandalf", [0,1,2,3,4,5,6,7], 1),
     ("gollum", [0,1,2,3,4,5,6,7], 1),
 ]
 
-label = "COMA_refactor_test_3"
+label = "COMA_refactor_test_4"
 config = "coma"
 env_config = "sc2"
 
@@ -14,59 +13,25 @@ n_repeat = 5
 param_dicts = []
 
 shared_params = {
-    "t_max": 1000000,
+    "t_max": 1500000,
 }
 
 extend_param_dicts(param_dicts, shared_params,
     {
-        "name": "coma_sc2_5m_agent_2e-5",
-        "lr": 0.0002,
+        "name": "coma_sc2_5m_lam0.8",
+        "lr": 0.0005,
         "critic_lr": 0.0005,
+        "td_lambda": 0.8,
         "env_args.map_name": "5m_5m"
     },
     repeats=4)
 
 extend_param_dicts(param_dicts, shared_params,
     {
-        "name": "coma_sc2_5m_both_2e-5",
-        "lr": 0.0002,
-        "critic_lr": 0.0002,
+        "name": "coma_sc2_5m_lam0.0",
+        "lr": 0.0005,
+        "critic_lr": 0.0005,
+        "td_lambda": 0.0,
         "env_args.map_name": "5m_5m"
     },
     repeats=4)
-
-extend_param_dicts(param_dicts, shared_params,
-    {
-        "name": "coma_sc2_5m_gamble",
-        "lr": 0.0002,
-        "critic_lr": 0.0002,
-        "batch_size": 16,
-        "batch_size_run": 16,
-        "buffer_size": 16,
-        "epsilon_start": 0.05,
-        "epsilon_finish": 0.05,
-        "env_args.map_name": "5m_5m"
-    },
-    repeats=3)
-
-extend_param_dicts(param_dicts, shared_params,
-    {
-        "name": "coma_sc2_5m_eps05",
-        "lr": 0.0005,
-        "critic_lr": 0.0005,
-        "epsilon_start": 0.05,
-        "epsilon_finish": 0.05,
-        "env_args.map_name": "5m_5m"
-    },
-    repeats=3)
-
-
-extend_param_dicts(param_dicts, shared_params,
-    {
-        "name": "coma_sc2_5m_target10k",
-        "lr": 0.0005,
-        "critic_lr": 0.0005,
-        "target_update_interval": 10000,
-        "env_args.map_name": "5m_5m"
-    },
-    repeats=2)
