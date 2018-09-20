a8da5635d28bc4715c00afab6434e2bc8bf71567
Tabish Rashid <tabzraz@gmail.com>
2018-09-20 22:31:31 +0100

    Lots of changes. 1) Fixing the COMA learner to pass the correct values when using the new td lambda targets 2) Agents now select an action in the last state stored, so we can bootstrap using that state in SARSA 3) Fixed/changed old td lambda targets to be more correct. They match the new ones with and without episode truncation on a quick pred_prey test.

--- src/utils/rl_utils.py
+++ src/utils/rl_utils.py
@@ -4,19 +4,17 @@ import torch as th
 def build_td_lambda_targets__old(rewards, terminated, mask, target_qs, n_agents, gamma, td_lambda):
     bs = rewards.size(0)
     max_t = rewards.size(1)
-    targets = rewards.new(target_qs.size()).zero_()
+    targets = rewards.new(target_qs.size()).zero_()[:,:-1] # Produce 1 less target than the inputted Q-Values
     running_target = rewards.new(bs, n_agents).zero_()
     terminated = terminated.float()
     for t in reversed(range(max_t)):
         if t == max_t - 1:
-            running_target = mask[:, t] * (rewards[:, t] + gamma * (1 - terminated[:, t]) * target_qs[:, t])
+            running_target = mask[:, t] * (rewards[:, t] + gamma * (1 - terminated[:, t]) * target_qs[:, t + 1])
         else:
             running_target = mask[:, t] * (
-                terminated[:, t] * rewards[:, t]
-                + (1 - terminated[:, t]) * (rewards[:, t] + gamma * (
-                               td_lambda * running_target
-                               + (1 - td_lambda) * target_qs[:, t])
-                                           ))
+                    terminated[:, t] * rewards[:, t]  # Just the reward if the env terminates
+                 + (1 - terminated[:, t]) * (rewards[:, t] + gamma * (td_lambda * running_target + (1 - td_lambda) * target_qs[:, t + 1]))
+            )
         targets[:, t, :] = running_target
     return targets
 
