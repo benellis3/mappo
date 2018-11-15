9be5a46900c19ffaff1120d1022b8572924d4055
Mikayel Samvelyan <mika.samvelyan@gmail.com>
2018-11-15 21:49:05 +0400

    changes in replay script

--- exp_scripts/save_replays.py
+++ exp_scripts/save_replays.py
@@ -3,8 +3,8 @@ import os
 import pprint
 
 maps = {
-    "micro_corridor" : {"qmix" : "qmix__micro_corridor__2018-11-14_10-29-41",
-                        "iql": "iql__micro_corridor__2018"},
+    "micro_corridor" : {"qmix" : "qmix__micro_corridor__2018-11-11_14-14-18",
+                        "iql": "iql__micro_corridor__2018-11-12_12-42-10"},
     "3s5z_3s6z": {"qmix": "qmix_parallel__2018-11-08_01-26-50"},
     "MMM2": {"qmix": "qmix__MMM2__2018-11-14_13-53-07"},
     "3s_5z": {"coma": "coma__3s_5z__2018-11-11_17-16-49",
@@ -23,7 +23,7 @@ maps = {
     "3m_3m": {"iql": "iql__3m_3m__2018-11-14_03-11-22",
               "coma": "coma__3m_3m__2018-11-14_07-00-54",
               "qmix": "qmix__3m_3m__2018-11-14_03-54-53"},
-    "3s_vs_3z": {"qmix": "qmix__3s_vs_3z__2018-11-14_04-39-31",
+    "3s_vs_3z": {"qmix": "qmix__3s_vs_3z__2018-11-14_20-36-01",
                  "iql": "iql__3s_vs_3z__2018-11-14_15-13-51",
                  "coma": "coma__3s_vs_3z__2018-11-14_08-15-09"},
     "3s_vs_4z": {"iql": "iql__3s_vs_4z__2018-11-11_10-51-00",
@@ -35,25 +35,25 @@ maps = {
             "qmix": "qmix__MMM__2018-11-14_14-21-08",
             "iql": "iql__MMM__2018-11-14_04-26-02"},
     "micro_2M_Z": {"iql": "iql__micro_2M_Z__2018-11-14_11-09-31",
-                   "coma": "coma__micro_2M_Z__2018-11-14_12-52-08",
-                   "qmix": "qmix_parallel_micro1__2018-10-19_10-39-33"},
-    "micro_baneling": {"iql": "iql__micro_baneling__2018-11-11_00-45-09",
+                   "coma": "coma__micro_2M_Z__2018-11-14_12-52-08"},
+                   #"qmix": "qmix_parallel_micro1__2018-10-19_10-39-33"}, we have this already
+    "micro_baneling": {#"iql": "iql__micro_baneling__2018-11-11_00-45-09",
                        "coma": "coma__micro_baneling__2018-11-12_16-55-00",
                        "qmix": "qmix__micro_baneling__2018-11-14_06-21-39"},
     "micro_colossus2" : {"qmix": "qmix__micro_colossus2__2018-11-13_22-01-06"},
     "micro_focus" : {"qmix": "qmix__micro_focus__2018-11-14_07-47-00"},
-    "micro_retarget" : {"qmix": "qmix__micro_retarget__2018-11-14_09-19-34",
+    "micro_retarget" : {"qmix": "qmix__micro_retarget__2018-11-11_02-36-11",
                         "iql": "iql__micro_retarget__2018-11-12_17-46-16",
                         "coma": "coma__micro_retarget__2018-11-12_14-43-52"}
 }
 
 pprint.pprint(maps)
 
-dir_prefix = "/Users/mika/Downloads/models"
+dir_prefix = "/Users/mika/Downloads/models2"
 
 for i, map_name in enumerate(maps.keys()):
 
-    print("\n", " Map {} - {}/{} ".format(map_name, i, len(maps)).center(60, "*"), "\n")
+    #print("\n", " Map {} - {}/{} ".format(map_name, i, len(maps)).center(60, "*"), "\n")
 
     experiment = maps[map_name]
 
@@ -64,7 +64,7 @@ for i, map_name in enumerate(maps.keys()):
         load_step = 10**7
         test_nepisode = 5
 
-        command = "python3 src/main.py --config={} --env-config=sc2 with env_args.map_name={} checkpoint_path={} load_step={} evaluate=True save_replay=True test_nepisode={} env_args.save_replay_prefix={}".format(
+        command = "python3 src/main.py --config={} --env-config=sc2 with env_args.map_name={} checkpoint_path={} load_step={} evaluate=True save_replay=True test_nepisode={} env_args.save_replay_prefix={} no-mongo=True runner=episode".format(
             method,
             map_name,
             checkpoint_path,
@@ -73,7 +73,10 @@ for i, map_name in enumerate(maps.keys()):
             method + '_' + map_name
         )
 
+        if not os.path.isdir(checkpoint_path):
+            print("Model {} does not exist!".format(checkpoint_path))
+
         print("Executing the following command:\n", command)
 
-        #subprocess.Popen(command)
+        subprocess.call(command.split())
 
