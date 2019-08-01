db87a9b2759b350ec20c517b46dd2445076bd362
Tabish Rashid <tabzraz@gmail.com>
2019-08-01 16:45:37 +0100

    coma runs

---
+++ exp_scripts/journal/coma_3.py
@@ -0,0 +1,46 @@
+from run_experiment import extend_param_dicts
+
+server_list = [
+    ("hydra", [0,1], 1),
+]
+
+label = "coma__19_July_2019_v1"
+config = "coma_smac"
+env_config = "sc2"
+
+n_repeat = 2 # Just incase some die
+
+parallel_repeat = 1
+
+param_dicts = []
+
+shared_params = {
+    "t_max": 2 * 1000 * 1000 + 50 * 1000,
+    "test_interval": 2000,
+    "test_nepisode": 32,
+    "test_greedy": True,
+    "env_args.obs_own_health": True,
+    "save_model": True,
+    "save_model_interval": 2000 * 1000,
+    "test_interval": 10000,
+    "log_interval": 10000,
+    "runner_log_interval": 10000,
+    "learner_log_interval": 10000,
+    "buffer_cpu_only": False,
+}
+
+maps = []
+
+# Easier maps
+maps += ["3s5z"]
+
+for map_name in maps:
+
+    name = "coma__{}".format(map_name)
+    extend_param_dicts(param_dicts, shared_params,
+        {
+            "name": name,
+            "env_args.map_name": map_name
+        },
+        repeats=parallel_repeat)
+
