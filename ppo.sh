d60a0b095bae76fefa3f2785931d08a54873f44e
mingfeisun <mingfei.sun.hk@gmail.com>
2020-05-20 09:45:53 +0800

    add ppo learner

---
+++ ppo.sh
@@ -0,0 +1,1 @@
+python3 src/main.py --config=ppo --env-config=sc2 with name=ppo_sc2
--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -2,6 +2,7 @@ from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .actor_critic_learner import ActorCriticLearner
 from .qtran_learner import QLearner as QTranLearner
+from .ppo_learner import PPOLearner
 
 REGISTRY = {}
 
@@ -9,3 +10,4 @@ REGISTRY["q_learner"] = QLearner
 REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["actor_critic_learner"] = ActorCriticLearner
+REGISTRY["ppo_learner"] = PPOLearner
