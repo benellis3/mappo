acbf497a7a3c743634820605c7eccaa3a8d3c76c
Mingfei Sun <mingfei.sun@ust.hk>
2020-10-24 11:02:13 +0100

    update

--- fetch_remote_results.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -chavzP --stats ${1}@${2}:~/projects/pymarl/results ./
--- ppo_conv1d.sh
+++
@@ -1,1 +0,0 @@
-python3 src/main.py --config=ppo_conv1d --env-config=sc2framestack with name=test__ppo_2m_vs_1z_2020_05_21__0
