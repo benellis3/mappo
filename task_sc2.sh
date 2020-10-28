cd55f234a2ed74fcc28ddfe26ce78becc4b2dd5f
Mingfei Sun <minsun@brown.cs.ox.ac.uk>
2020-10-28 15:27:14 +0000

    update

---
+++ task_sc2.sh
@@ -0,0 +1,11 @@
+# ./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
+# ./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
+# ./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
+# ./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
+# ./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
+
+./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
+./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
