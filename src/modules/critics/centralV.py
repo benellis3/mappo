6a83dbcbf82fcf77969c0de15474be27ba38a4db
Mingfei Sun <mingfei.sun@ust.hk>
2020-11-02 10:36:15 +0000

    update

--- src/modules/critics/centralV.py
+++ src/modules/critics/centralV.py
@@ -1,6 +1,8 @@
+import numpy as np
 import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
+from components.running_mean_std import RunningMeanStd
 
 
 class CentralVCritic(nn.Module):
@@ -14,6 +16,12 @@ class CentralVCritic(nn.Module):
         input_shape = self._get_input_shape(scheme)
         self.output_type = "v"
 
+        if getattr(args, "is_observation_normalized", None):
+            self.is_obs_normalized = True
+            self.obs_rms = RunningMeanStd(shape=np.prod(input_shape))
+        else:
+            self.is_obs_normalized = False
+
         # Set up network layers
         self.fc1 = nn.Linear(input_shape, 128)
         self.fc2 = nn.Linear(128, 128)
@@ -21,16 +29,17 @@ class CentralVCritic(nn.Module):
 
     def forward(self, batch, t=None):
         inputs, bs, max_t = self._build_inputs(batch, t=t)
+
+        if self.is_obs_normalized: 
+            inputs = (inputs - self.obs_rms.mean) / th.sqrt(self.obs_rms.var)
+
         x = F.relu(self.fc1(inputs))
         x = F.relu(self.fc2(x))
         q = self.fc3(x)
         return q.view(bs, max_t, 1, -1).repeat(1, 1, self.n_agents, 1)
 
-    def forward_obs(self, inputs):
-        x = F.relu(self.fc1(inputs))
-        x = F.relu(self.fc2(x))
-        q = self.fc3(x)
-        return q
+    def update_rms(self, batch_obs):
+        self.obs_rms.update(batch_obs)
 
     def _build_inputs(self, batch, t=None):
         bs = batch.batch_size
@@ -40,28 +49,27 @@ class CentralVCritic(nn.Module):
         # state
         inputs.append(batch["state"][:, ts])
 
-        if self.args.obs_last_action:
-            if t == 0:
-                inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
-            else:
-                inputs.append(batch["actions_onehot"][:, t-1])
-        if self.args.obs_agent_id:
-            inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
+        # observations
+        inputs.append(batch["obs"][:, ts].view(bs, max_t, -1))
 
-        inputs = th.cat([x.reshape(bs*self.n_agents, -1) for x in inputs], dim=1)
+        # last actions
+        if t == 0:
+            inputs.append(th.zeros_like(batch["actions_onehot"][:, 0:1]).view(bs, max_t, 1, -1))
+        elif isinstance(t, int):
+            inputs.append(batch["actions_onehot"][:, slice(t-1, t)].view(bs, max_t, 1, -1))
+        else:
+            last_actions = th.cat([th.zeros_like(batch["actions_onehot"][:, 0:1]), batch["actions_onehot"][:, :-1]], dim=1)
+            last_actions = last_actions.view(bs, max_t, 1, -1)
+            inputs.append(last_actions)
 
+        inputs = th.cat([x.reshape(bs * max_t, -1) for x in inputs], dim=1)
         return inputs, bs, max_t
 
     def _get_input_shape(self, scheme):
         # state
         input_shape = scheme["state"]["vshape"]
-
-        # last action
-        if getattr(self.args, 'obs_last_action', None):
-            input_shape += scheme["actions_onehot"]["vshape"][0]
-
-        # agent id
-        if getattr(self.args, 'obs_agent_id', None):
-            input_shape += self.n_agents
-            
+        # observations
+        input_shape += scheme["obs"]["vshape"] * self.n_agents
+        # last actions
+        input_shape += scheme["actions_onehot"]["vshape"][0] * self.n_agents
         return input_shape
