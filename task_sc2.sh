3c95e92696f63cb0b0fa4969186417e8be0d4bb7
Mingfei Sun <minsun@savitar.cs.ox.ac.uk>
2020-11-02 10:47:36 +0000

    update

--- task_sc2.sh
+++ task_sc2.sh
@@ -4,8 +4,22 @@
 # ./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
 # ./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" name="ppo" label="ppo_sc2_corridor_mingfei" &
 
-./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
-./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
-./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
-./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
-./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" name="ppo" label="ppo_sc2_6h_vs_8z_mingfei" &
+./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s3z" name="ppo_cv" label="ppo_sc2_2s3z_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s3z" name="ppo_cv" label="ppo_sc2_2s3z_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s3z" name="ppo_cv" label="ppo_sc2_2s3z_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s3z" name="ppo_cv" label="ppo_sc2_2s3z_mingfei" &
+
+./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="bane_vs_bane" name="ppo_cv" label="ppo_sc2_bane_vs_bane_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="bane_vs_bane" name="ppo_cv" label="ppo_sc2_bane_vs_bane_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="bane_vs_bane" name="ppo_cv" label="ppo_sc2_bane_vs_bane_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="bane_vs_bane" name="ppo_cv" label="ppo_sc2_bane_vs_bane_mingfei" &
+
+./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="3m" name="ppo_cv" label="ppo_sc2_3m_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="3m" name="ppo_cv" label="ppo_sc2_3m_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="3m" name="ppo_cv" label="ppo_sc2_3m_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="3m" name="ppo_cv" label="ppo_sc2_3m_mingfei" &
+
+./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s_vs_1sc" name="ppo_cv" label="ppo_sc2_2s_vs_1sc_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s_vs_1sc" name="ppo_cv" label="ppo_sc2_2s_vs_1sc_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s_vs_1sc" name="ppo_cv" label="ppo_sc2_2s_vs_1sc_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with is_central_value=True env_args.map_name="2s_vs_1sc" name="ppo_cv" label="ppo_sc2_2s_vs_1sc_mingfei" &
