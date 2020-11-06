f41b7235b31ca6b5c7173e1f659a61327d67909c
Mingfei Sun <mingfei.sun@ust.hk>
2020-11-06 10:56:36 +0000

    add rnn critic

--- src/modules/critics/__init__.py
+++ src/modules/critics/__init__.py
@@ -3,7 +3,9 @@ critic_REGISTRY = {}
 from .centralV import CentralVCritic
 from .conv1d_critic import Conv1dCritic
 from .vanilla_critic import VanillaCritic
+from .rnn_critic import RNNCritic
 
 critic_REGISTRY["vanilla"] = VanillaCritic
+critic_REGISTRY["rnn"] = RNNCritic
 critic_REGISTRY["central_v"] = CentralVCritic
 critic_REGISTRY["conv1d_critic"] = Conv1dCritic
---
+++ src/modules/critics/rnn_critic.py
@@ -0,0 +1,69 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from modules.agents import REGISTRY as agent_REGISTRY
+import numpy as np
+
+
+class RNNCritic(nn.Module):
+    def __init__(self, scheme, args):
+        super(RNNCritic, self).__init__()
+
+        self.args = args
+        self.n_actions = args.n_actions
+        self.n_agents = args.n_agents
+        self.state_dim = int(np.prod(args.state_shape))
+
+        input_shape = self._get_input_shape(scheme)
+
+        self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
+        self.rnn = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
+        self.fc2 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
+
+    def init_hidden(self):
+        # make hidden states on same device as model
+        return self.fc1.weight.new(1, self.args.rnn_hidden_dim).zero_()
+
+    def forward(self, batch):
+        hidden_states = self.init_hidden().unsqueeze(0).expand(batch.batch_size, self.n_agents, -1)  # bav
+
+        outputs = []
+
+        for t in range(batch.max_seq_length):
+            inputs = self._build_inputs(batch, t)
+            x = F.relu(self.fc1(inputs))
+            h_in = hidden_states.reshape(-1, self.args.rnn_hidden_dim)
+            hidden_states = h_in
+            h = self.rnn(x, h_in)
+            q = self.fc2(h)
+
+            actions_taken_t = batch["actions"][:,t].reshape(-1,1)
+            qs_taken = th.gather(q, dim=1, index=actions_taken_t).reshape(batch.batch_size, -1)
+            qs_taken_repeat = qs_taken.repeat(1, self.n_agents).reshape(-1, self.n_agents)
+
+            outputs.append(qs_taken_repeat.reshape(batch.batch_size, self.n_agents, self.n_actions))
+
+        output_tensor = th.stack(outputs, dim=1)
+        return output_tensor
+
+    def _build_inputs(self, batch, t):
+        # Assumes homogenous agents with flat observations.
+        # Other MACs might want to e.g. delegate building inputs to each agent
+        bs = batch.batch_size
+        inputs = []
+        inputs.append(batch["obs"][:, t])  # b1av
+        if self.args.obs_last_action:
+            if t == 0:
+                inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
+            else:
+                inputs.append(batch["actions_onehot"][:, t-1])
+
+        inputs = th.cat([x.reshape(bs*self.n_agents, -1) for x in inputs], dim=1)
+        return inputs
+
+    def _get_input_shape(self, scheme):
+        input_shape = scheme["obs"]["vshape"]
+        if self.args.obs_last_action:
+            input_shape += scheme["actions_onehot"]["vshape"][0]
+
+        return input_shape
