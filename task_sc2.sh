c21049f87b8a26bca59e05be4fe097768f49441f
Mingfei Sun <minsun@savitar.cs.ox.ac.uk>
2020-12-29 12:36:58 +0000

    add new smac and tasks

--- task_sc2.sh
+++ task_sc2.sh
@@ -1,14 +1,39 @@
-./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s3z" name="ppo_rnn2" label="ppo_sc2_2s3z_mingfei" &
-./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s3z" name="ppo_rnn2" label="ppo_sc2_2s3z_mingfei" &
-./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s3z" name="ppo_rnn2" label="ppo_sc2_2s3z_mingfei" &
-./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s3z" name="ppo_rnn2" label="ppo_sc2_2s3z_mingfei" &
+./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_rnn_new" label="sc2_3m_new_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_rnn_new" label="sc2_3m_new_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_rnn_new" label="sc2_3m_new_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_rnn_new" label="sc2_3m_new_mingfei" &
 
-./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" name="ppo_rnn2" label="ppo_sc2_3m_mingfei" &
-./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" name="ppo_rnn2" label="ppo_sc2_3m_mingfei" &
-./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" name="ppo_rnn2" label="ppo_sc2_3m_mingfei" &
-./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3m" name="ppo_rnn2" label="ppo_sc2_3m_mingfei" &
+./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_rnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_rnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_rnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_rnn_new" label="ppo_sc2_3s5z_new_mingfei" &
 
-./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s_vs_1sc" name="ppo_rnn2" label="ppo_sc2_2s_vs_1sc_mingfei" &
-./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s_vs_1sc" name="ppo_rnn2" label="ppo_sc2_2s_vs_1sc_mingfei" &
-./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s_vs_1sc" name="ppo_rnn2" label="ppo_sc2_2s_vs_1sc_mingfei" &
-./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="2s_vs_1sc" name="ppo_rnn2" label="ppo_sc2_2s_vs_1sc_mingfei" &
+# ./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_rnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+# ./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_rnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+# ./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_rnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+# ./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_rnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+# 
+# ./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_6h_vs_8z_new_mingfei" &
+# ./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_6h_vs_8z_new_mingfei" &
+# ./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_6h_vs_8z_new_mingfei" &
+# ./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_6h_vs_8z_new_mingfei" &
+# 
+# ./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_corridor_new_mingfei" &
+# ./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_corridor_new_mingfei" &
+# ./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_corridor_new_mingfei" &
+# ./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_corridor_new_mingfei" &
+# 
+# ./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="1c3s5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_1c3s5z_new_mingfei" &
+# ./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="1c3s5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_1c3s5z_new_mingfei" &
+# ./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="1c3s5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_1c3s5z_new_mingfei" &
+# ./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="1c3s5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_1c3s5z_new_mingfei" &
+# 
+# ./run.sh 0 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_3s_vs_5z_new_mingfei" &
+# ./run.sh 1 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_3s_vs_5z_new_mingfei" &
+# ./run.sh 2 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_3s_vs_5z_new_mingfei" &
+# ./run.sh 3 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_3s_vs_5z_new_mingfei" &
+# 
+# ./run.sh 4 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_MMM2_new_mingfei" &
+# ./run.sh 5 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_MMM2_new_mingfei" &
+# ./run.sh 6 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_MMM2_new_mingfei" &
+# ./run.sh 7 python3 src/main.py --config=ppo_rnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_rnn_new" label="ppo_sc2_MMM2_new_mingfei" &
