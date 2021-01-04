3411daa0e29029e704cb8329bac135b139ce8b06
Mingfei Sun <minsun@dgx1.cs.ox.ac.uk>
2021-01-04 21:01:03 +0000

    add tag

--- task_sc2.sh
+++ task_sc2.sh
@@ -1,32 +1,32 @@
-./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn" label="ppo_sc2_3s5z_new_mingfei" &
-./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn" label="ppo_sc2_3s5z_new_mingfei" &
-./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn" label="ppo_sc2_3s5z_new_mingfei" &
-./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn_new" label="ppo_sc2_3s5z_new_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z" t_max=2000000 name="ppo_cnn_new" label="ppo_sc2_3s5z_new_mingfei" &
 
-./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn" label="sc2_3s5z_vs_3s6z_new_mingfei" &
-./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn" label="sc2_3s5z_vs_3s6z_new_mingfei" &
-./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn" label="sc2_3s5z_vs_3s6z_new_mingfei" &
-./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s5z_vs_3s6z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s5z_vs_3s6z_new_mingfei" &
 
-./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn" label="sc2_6h_vs_8z_new_mingfei" &
-./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn" label="sc2_6h_vs_8z_new_mingfei" &
-./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn" label="sc2_6h_vs_8z_new_mingfei" &
-./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn" label="sc2_6h_vs_8z_new_mingfei" &
+./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn_new" label="sc2_6h_vs_8z_new_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn_new" label="sc2_6h_vs_8z_new_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn_new" label="sc2_6h_vs_8z_new_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="6h_vs_8z" t_max=10000000 name="ppo_cnn_new" label="sc2_6h_vs_8z_new_mingfei" &
 
-./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn" label="sc2_corridor_new_mingfei" &
-./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn" label="sc2_corridor_new_mingfei" &
-./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn" label="sc2_corridor_new_mingfei" &
-./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn" label="sc2_corridor_new_mingfei" &
+./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn_new" label="sc2_corridor_new_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn_new" label="sc2_corridor_new_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn_new" label="sc2_corridor_new_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="corridor" t_max=10000000 name="ppo_cnn_new" label="sc2_corridor_new_mingfei" &
 
-./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn" label="sc2_3s_vs_5z_new_mingfei" &
-./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn" label="sc2_3s_vs_5z_new_mingfei" &
-./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn" label="sc2_3s_vs_5z_new_mingfei" &
-./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn" label="sc2_3s_vs_5z_new_mingfei" &
+./run.sh 0 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s_vs_5z_new_mingfei" &
+./run.sh 1 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s_vs_5z_new_mingfei" &
+./run.sh 2 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s_vs_5z_new_mingfei" &
+./run.sh 3 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3s_vs_5z" t_max=10000000 name="ppo_cnn_new" label="sc2_3s_vs_5z_new_mingfei" &
 
-./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn" label="sc2_MMM2_new_mingfei" &
-./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn" label="sc2_MMM2_new_mingfei" &
-./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn" label="sc2_MMM2_new_mingfei" &
-./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn" label="sc2_MMM2_new_mingfei" &
+./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn_new" label="sc2_MMM2_new_mingfei" &
+./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn_new" label="sc2_MMM2_new_mingfei" &
+./run.sh 6 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn_new" label="sc2_MMM2_new_mingfei" &
+./run.sh 7 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="MMM2" t_max=10000000 name="ppo_cnn_new" label="sc2_MMM2_new_mingfei" &
 
 ./run.sh 4 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_cnn_new" label="sc2_3m_new_mingfei" &
 ./run.sh 5 python3 src/main.py --config=ppo_cnn --env-config=sc2 with env_args.map_name="3m" t_max=2000000 name="ppo_cnn_new" label="sc2_3m_new_mingfei" &
