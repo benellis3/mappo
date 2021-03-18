92e0ed3791442646e6925bb7388eb24a1f9e7f01
Benjamin Ellis <benlis@gimli.cs.ox.ac.uk>
2021-03-18 11:05:25 +0000

    fix syntax error

--- src/modules/critics/cnn_critic.py
+++ src/modules/critics/cnn_critic.py
@@ -27,7 +27,7 @@ class CNNCritic(nn.Module):
         self.cnn2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0)
         self.cnn3 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0)
 
-        input_dim = (input_shape - 1) // 2 - 3 see # https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
+        input_dim = (input_shape - 1) // 2 - 3 # see https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
         self.fc1 = nn.Linear(256 * input_dim, 128)
         self.fc2 = nn.Linear(128, 1)
 
