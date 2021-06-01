6d0f03fca5578011897f9cd23252a1abd3ea67fa
Mingfei Sun <minsun@dgx1.cs.ox.ac.uk>
2021-06-01 10:54:24 +0100

    add vdn critic

--- src/modules/critics/__init__.py
+++ src/modules/critics/__init__.py
@@ -3,11 +3,13 @@ critic_REGISTRY = {}
 from .centralV import CentralVCritic
 from .vanilla_critic import VanillaCritic
 from .rnn_critic import RNNCritic
+from .rnn_critic_sum import RNNCriticSum
 from .cnn_critic import CNNCritic
 from .central_critic import CentralCritic
 
 critic_REGISTRY["vanilla"] = VanillaCritic
 critic_REGISTRY["rnn"] = RNNCritic
+critic_REGISTRY["rnn_sum"] = RNNCriticSum
 critic_REGISTRY["central_v"] = CentralVCritic
 critic_REGISTRY["cnn"] = CNNCritic
 critic_REGISTRY["central_critic"] = CentralCritic
