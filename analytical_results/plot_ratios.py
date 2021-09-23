544117dcc6240f4d7bdc86e0b18102a6a9572e24
Mingfei <mingfei.sun.hk@gmail.com>
2021-09-23 16:40:35 +0100

    add plot scripts

--- analytical_results/plot_ratios.py
+++ analytical_results/plot_ratios.py
@@ -44,35 +44,54 @@ def plot_ratios_clip_range(res_df, actor_lr=0.0005, num_epochs=10):
     res_df = res_df[res_df['learning_rate'] == actor_lr]
     res_df = res_df[res_df['num_epochs'] == num_epochs]
 
+    all_items = []
     clip_range = res_df['clip_range']
-    ratios_min = res_df['ratios_min']
-    ratios_mean = res_df['ratios_mean']
-    ratios_max = res_df['ratios_max']
+    ratios = res_df['ratios']
+    for i in range(len(clip_range)):
+        ratio_list = ratios.iloc[i].split(' ')
+        ratio_list = [float(s) for s in ratio_list]
+        # randomly sample 1000 points
+        ratio_list = np.random.choice(ratio_list, 1000, replace=False)
+        for j in ratio_list:
+            all_items.append({'clip_range': clip_range.iloc[i], 'ratio': j})
     
-    clip_range = pd.concat([clip_range, clip_range, clip_range])
-    ratios = pd.concat([ratios_min, ratios_mean, ratios_max])
+    df_ratios = pd.DataFrame(all_items)
 
-    # clip_range = pd.concat([clip_range, clip_range])
-    # ratios = pd.concat([ratios_min, ratios_max])
+    sns_plot = sns.catplot(x="clip_range", y="ratio", jitter=True, s=2, data=df_ratios)
+    # sns_plot = sns.catplot(x="clip_range", y="ratio", kind="violin", 
+    #                        bw=.2, jitter=True, data=df_ratios)
+    # sns.swarmplot(x="clip_range", y="ratio", color="k", size=3, data=df_ratios, ax=sns_plot.ax)
 
-    df_ratios = pd.DataFrame({'clip_range': clip_range, 'ratios': ratios})
+    sns_plot.set(xlabel='Clipping range', ylabel='Ratio distribution')
+    sns_plot.savefig('figures/ratios_with_clip_range_epoch_%d.pdf'%num_epochs)
 
-    # sns_plot = sns.catplot(x="clip_range", y="ratios", jitter=True, data=df_ratios)
-    sns_plot = sns.catplot(x="clip_range", y="ratios", kind="violin", bw=.3, jitter=True, data=df_ratios)
-    sns.swarmplot(x="clip_range", y="ratios", color="k", size=3, data=df_ratios, ax=sns_plot.ax)
-
-    sns_plot.set(xlabel='Clipping range', ylabel='Ratios')
-    sns_plot.savefig('figures/ratios_with_clip_range.pdf')
-
-def plot_tmp(res_df, actor_lr=0.0005, num_epochs=8):
+def plot_ratio_range_trust_region(res_df, actor_lr=0.0005, num_epochs=10):
     res_df = res_df[res_df['learning_rate'] == actor_lr]
-    res_df = res_df[res_df['num_epochs'] == num_epochs]
+    # res_df = res_df[res_df['num_epochs'] == num_epochs]
+    res_df = res_df[res_df['clip_range'] == 0.1]
+    epsilon = res_df['epsilon_max']
+    approx_TV = res_df['independent_approx_TV']
+    sns_plot = sns.regplot(x=epsilon, y=approx_TV, marker="+")
+    sns_plot = sns_plot.get_figure()
+    ax = sns_plot.axes[0]
+    y = epsilon
+    ax.plot(epsilon, y, linestyle=':', color='gray')
 
-    sns_plot = sns.relplot(x="clip_range", y="ratios_max", kind="line", 
-                hue="num_epochs", col="learning_rate", data=res_df, palette="tab10",)
+    sns_plot.savefig('figures/ratio_range_trust_region.pdf')
 
-    sns_plot.set(xlabel='Clipping range', ylabel='Ratios')
-    sns_plot.savefig('figures/ratios_with_clip_range.pdf')
+def plot_ratio_range_joint_trust_region(res_df, actor_lr=0.0005, num_epochs=10):
+    res_df = res_df[res_df['learning_rate'] == actor_lr]
+    # res_df = res_df[res_df['num_epochs'] == num_epochs]
+    res_df = res_df[res_df['clip_range'] == 0.1]
+    epsilon_sum = res_df['epsilon_sum']
+    approx_TV = res_df['joint_approx_TV']
+    sns_plot = sns.regplot(x=epsilon_sum, y=approx_TV, marker="+")
+    sns_plot = sns_plot.get_figure()
+    ax = sns_plot.axes[0]
+    y = epsilon_sum
+    ax.plot(epsilon_sum, y, linestyle=':', color='gray')
+
+    sns_plot.savefig('figures/ratio_range_trust_region.pdf')
 
 def main():
     dir_path = '2s3z'
@@ -99,7 +118,10 @@ def main():
     # plot_ratios_range(result_df)
 
     ### plot ratios vs clip range
-    plot_ratios_clip_range(result_df)
+    # plot_ratios_clip_range(result_df)
+
+    ### plot trust region vs ratios
+    plot_ratio_range_trust_region(result_df)
     
 if __name__ == '__main__':
     main()
