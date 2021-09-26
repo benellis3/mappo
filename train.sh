196d982b267a1a0f8cf7c6423e9a47e4674c4367
Mingfei Sun <minsun@dgx1.cs.ox.ac.uk>
2021-09-26 22:22:40 +0100

    update tasks

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-cat task.txt | xargs -n 6 -P 20 \
+cat task.txt | xargs -n 6 -P 18 \
     sh -c 'bash run.sh $0 python3 src/main.py --config=$1 --env-config=sc2 with env_args.map_name=$2 t_max=$3 name=$4 label=$5'
